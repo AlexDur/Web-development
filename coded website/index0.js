e4295073be8ec2a7999cb9497735a7600857badb
Alexander Durach <69761225+AlexDur@users.noreply.github.com>
2021-11-10 00:52:37 +0100

    scss and layout

--- coded website/index0.js
+++ coded website/index0.js
@@ -61,16 +61,51 @@ const btnScrollToTop = document.querySelector("#btnScrollUp");
 window.addEventListener("scroll", scrollFunction);
 
 function scrollFunction () {
-    if (window.pageYOffset > 300) {
-        btnScrollToTop.style.display = "block";
+    if (window.pageYOffset > 300) { // Show btnScrollToTop
+        if(!btnScrollToTop.classList.contains("btnEntrance")){
+            btnScrollToTop.classList.remove("btnExit");
+            btnScrollToTop.classList.add("btnEntrance");
+            btnScrollToTop.style.display = "block";
+       }
     }
     else { //Hide btnScrollToTop
-        btnScrollToTop.style.display = "none";
+        if(btnScrollToTop.classList.contains("btnEntrance")){
+        btnScrollToTop.classList.remove("btnEntrance");
+        btnScrollToTop.classList.add("btnExit");
+        setTimeout(function() {
+            btnScrollToTop.classList.add("btnEntrance");
+        }, 250);
+        }
     }
 }
 
-btnScrollToTop.addEventListener("click", backToTop)
+btnScrollToTop.addEventListener("click", backToTop);
 
 function backToTop () {
     window.scrollTo(0,0);
 }
+
+function smoothScrollBackToTop() {
+    const targetPosition = 0;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    /* const duration = 100000; */
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+        if(!start) start = timestamp;
+        const progress = timestamp - start;
+        window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+        if(progress < duration) window.requestAnimationFrame(step);
+    }
+}
+
+function easeInOutCubic(t, b ,c ,d) {
+    t /= d/2;
+    if(t < 1) return c/2*t*t*t + b;
+    t -= 2;
+    return c/2*(t*t*t + 2) + b;
+};
+
